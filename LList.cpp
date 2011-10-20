45754ed7ac90dc3613ac27e7d35e3f37db2c5209
dorsalfinsalsa <dorsalfinsalsa@c93f7471-7179-d273-4764-f64499a3d2c5>
2011-10-20 23:15:27 +0000

    Added some functionality to LList and added a Node class for LList (LList_Node)

--- LList.cpp
+++ LList.cpp
@@ -1,21 +1,49 @@
-#include"LList.h"
+#include "LList.h"
 
-LList::LList() {
-
+// Default constructor
+LList :: LList () {
+	head = NULL;
+	size = 0;
 } 
 
-LList::LList(int x) {
+// Overridden constructor
+LList :: LList (int x) {
+	head = new LList_Node (x);
+	size = 1;
+}
+
+// Destructor
+LList :: ~LList() {
+	// Only need to something if there are nodes left to delete
+	while (size > 0) {
+		this -> remove_node ();
+	}
+}
+
+// Automatically adds 1 through upper to the list
+void LList :: populate_list (int upper) {
+	for (int i = 0; i < upper; i++) {
+
+	}
+}
+
+// Prints everything in the linked list
+void LList :: printList () {
 
 }
 
-LList::~LList() {
+// Prints x first values of the linked list
+void LList :: printList(int x) {
 
 }
 
-void LList::printList() {
+// Removes the head node
+void LList :: remove_node () {
+	this -> remove_node (0);
+}
+
+// Removes a node at index
+void LList :: remove_node (int index) {
 
 }
 
-void LList::printList(int x) {
-
-}
