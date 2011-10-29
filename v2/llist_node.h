98c7e8408b4825c4c67c55a03dadfe0942623518
altomnr <altomnr@gmail.com>
2011-10-29 08:27:07 +0000

    Added functionality to llist.cc, also re-added llist_node files for the LList class

---
+++ v2/llist_node.h
@@ -0,0 +1,25 @@
+#ifndef LLIST_NODE_H
+#define LLIST_NODE_H
+
+#include <iostream>
+
+using namespace std;
+
+class LList_Node {
+	public:
+		// allow LList to get access to our private members
+		friend class LList;
+
+		LList_Node ();
+		LList_Node (int);
+
+		bool has_next ();
+
+	protected:
+		LList_Node * next;
+
+	private:
+		int value;
+};
+
+#endif
