5b554d242661c7fad53b02d1862c58e55130d236
dorsalfinsalsa <dorsalfinsalsa@c93f7471-7179-d273-4764-f64499a3d2c5>
2011-10-30 19:50:57 +0000

    before adding sorting alg

--- v2/llist.cc
+++ v2/llist.cc
@@ -2,7 +2,6 @@
 
 //Constructor
 LList :: LList () {
-	cout << "LList :: Constructor called" << endl;
 	
 	head = NULL;
 	size = 0;
@@ -10,7 +9,6 @@ LList :: LList () {
 
 //Destructor
 LList :: ~LList () {
-	cout << "LList :: Destructor called" << endl;
 
 	while(size > 0) {
 		remove();
@@ -19,7 +17,6 @@ LList :: ~LList () {
 
 //Adds a Node to the list with value == value
 void LList :: add (int value) {
-	cout << "LList :: add () called" << endl;
 	
 	if (size == 0) {
 		head = new LList_Node (value);	
@@ -36,31 +33,30 @@ void LList :: add (int value) {
 	size++;
 }
 
+/* Sorts the list by doing a mergesort on the members */
 // TODO : Method
 void LList :: sort_list () {
-	cout << "LList :: sort_list () called" << endl;
+
 }
 
 //Prints the LList by iterating through the nodes and printing the value
 //of each, 20 per line;
 void LList :: print_list () {
-	cout << "LList :: print_list () called" << endl;
 	
-	if (size == 0) {
-		cout << "LList is empty!\n";
-	} else {
-		LList_Node * current_node = head;
+	LList_Node * current_node = head;
 
-		cout << "{";
+	cout << "{";
 
-		for (int i = 0; i < size - 1; i++) {
-			cout << current_node -> value << ", ";
-			current_node = current_node -> next;
-		}
-
-		cout << current_node -> value;
-		cout << "}" << endl;
+	for (int i = 0; i < size - 1; i++) {
+		cout << current_node -> value << ", ";
+		current_node = current_node -> next;
 	}
+
+	if (size > 0) {
+		cout << current_node -> value;
+	}
+
+	cout << "}" << endl;
 }
 
 //Called by Destructor
@@ -71,8 +67,7 @@ void LList :: remove () {
 	} else if(size > 1) {
 		LList_Node * to_delete = head;
 		head = head -> next;
-		
-		delete(to_delete);
+		delete (to_delete);
 		size--;	
 	}
 }
@@ -82,3 +77,35 @@ char * LList :: to_string () {
 	
 	return NULL;
 }
+
+/* Gets the value of the LList_Node at the given index */
+int LList :: get_node (int index) {
+
+	LList_Node * current_node = head;
+
+	for (int i = 0; i < index; i++) {
+		current_node = current_node -> next;
+	}
+
+	return current_node -> value;
+}
+
+LList_Node * LList :: get_head () {
+	return head;
+}
+
+int LList :: get_size () {
+	return size;
+}
+
+void LList :: print_list (int nodes) {
+
+	cout << "{";
+
+	for (int i = 0; i < nodes - 1; i++) {
+		cout << get_node (i) << ", ";
+	}
+
+	cout << get_node (nodes - 1) << "}" << endl;
+}
+
