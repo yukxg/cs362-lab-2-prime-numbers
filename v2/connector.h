0511444373fbd90d8818aed781c81a0038b67659
dorsalfinsalsa <dorsalfinsalsa@c93f7471-7179-d273-4764-f64499a3d2c5>
2011-10-31 06:48:25 +0000

    added functionality to the connector class and a bit of modifications elsewhere

--- v2/connector.h
+++ v2/connector.h
@@ -1,24 +1,48 @@
 #ifndef CONNECTOR_H
 #define CONNECTOR_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <iostream>
+#include <netdb.h>
+#include <string>
 
 using namespace std;
+
+#define BACKLOG 10 // how many pending connections queue will hold
+#define MAXDATASIZE 100 // max number of bytes we can get at once
+#define RECV_SIZE 50 // how many character to use for the receiver variable
 
 class Connector {
 
 	public:
 		Connector ();
 		~Connector ();
-
+		
 		void set_port (int);
 		void set_receiver (char *);
-		void send (char *);
-		void listen ();
+		void send_msg (char *);
+		void listen_msg ();
 		char * get_msg ();
-
+		
 	private:
-
+		friend void sigchld_handler (int);
+		void listen_msg_helper ();
+		
+		int port;
+		char * receiver;
+		char * msg;
 };
 
+
 #endif
+
