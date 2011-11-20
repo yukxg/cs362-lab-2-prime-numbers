c3fb134a58a607c77dd9ec1dc0b4455ccaa485b3
dorsalfinsalsa <dorsalfinsalsa@c93f7471-7179-d273-4764-f64499a3d2c5>
2011-11-20 22:36:26 +0000

    Allow connector to send more than 64KB of but isn't fully functional just yet

--- v2/connector.cc
+++ v2/connector.cc
@@ -1,10 +1,10 @@
 #include "connector.h"
+#include <sstream>
 
 Connector :: Connector (string addr_name, int port_num) {
 	msg = new string ();
 	addr = new string (addr_name);
 	port = port_num;
-	buf_size = 63000;
 	sock = new UDPSocket (port);
 }
 
@@ -15,22 +15,63 @@ Connector :: ~Connector () {
 }
 
 void Connector :: send_msg (string message) {
-	cout << "sending" << endl;
-	sock -> sendTo (message.c_str (), message.size (), * addr, port);
+	sleep (1);
+	int num_messages = 1;
+	int index = 0;
+	string msg2 = "";
+
+		// break the message into smaller packets
+	if (message.size () > MAX_MSG_SIZE) {
+		num_messages = (int) (message.size ()/ MAX_MSG_SIZE);
+		num_messages++;
+	}
+
+	cout << "number of messages to send: " << num_messages << endl;
+
+	// Tell listener how many messages to expect after this message
+	stringstream stream;
+	stream << num_messages << endl;
+	msg2 = stream.str ();
+	cout << "msg2: " << msg2 << endl;
+	sock -> sendTo (msg2.c_str (), MAX_MSG_SIZE, * addr, port);
+	cout << "sending: " << msg2 << endl;
+	
+	// Break message into parts and send
+	for (int i = 0; i < num_messages; i++) {
+		msg2 = "";
+		msg2 += message.substr (index, MAX_MSG_SIZE);
+		index += MAX_MSG_SIZE;
+		sock -> sendTo (msg2.c_str (), msg2.size (), * addr, port);
+		cout << "sending: " << msg2 << endl;
+	}
 }
 
 void Connector :: listen_msg () {
-	char buffer [buf_size];
+	cout << "listening" << endl;
 
-	cout << "listening..." << endl;
-	int bytes_rcvd = sock -> recvFrom (buffer, buf_size, * addr, port);
+	int num_messages = 1;
+	stringstream stream;
+		
+	char buffer [MAX_MSG_SIZE];
+	int bytes_rcvd = sock -> recvFrom (buffer, MAX_MSG_SIZE, * addr, port);
 	buffer [bytes_rcvd] = '\0';
-	* msg = buffer;
-	cout << "received: " << *msg << endl;
+	* msg += buffer;
+	
+	stream << num_messages;
+	num_messages = atoi (msg -> c_str ());
+	cout << "number of messages to expect: " << num_messages << endl;
+	* msg = "";
+
+	for (int i = 0; i < num_messages; i++) {
+		bytes_rcvd = sock -> recvFrom (buffer, MAX_MSG_SIZE, * addr, port);
+		buffer [bytes_rcvd] = '\0';
+		msg -> append (buffer);
+	}
+
+	cout << "message received: " << * msg << endl;
 }
 
-void Connector :: set_port (int port_num) {
-	port = port_num;
+void Connector :: set_port (int port_num) { port = port_num;
 }
 
 void Connector :: set_receiver (string addr_name) {
